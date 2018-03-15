97b15095b6f42f8177e60adde2c7647335b5c28e
Rajendra <rajendra.baviskar@cerner.com>
2018-03-15 11:25:55 -0500

    without thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 
-import reducer from './reducer';
+
 import { applyMiddleware, compose, createStore } from 'redux';
 import logger from 'redux-logger';
+import rootReducer from './reducers';
 //import { createBrowserHistory } from './../../Library/Caches/typescript/2.6/node_modules/@types/history';
 
 // Todo : add middleware
@@ -9,6 +10,6 @@ let finalCreateStore  = compose (
   applyMiddleware(logger())  
 )(createStore);
 
-export default function configureStore(initialState = {todos: []}) {
-    return finalCreateStore(reducer, initialState);
+export default function configureStore(initialState = {todos: [], user:{} }) {
+    return finalCreateStore(rootReducer, initialState);
 }
